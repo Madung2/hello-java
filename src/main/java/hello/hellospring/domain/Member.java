e003d8386a80056dac77faa321c4015599f45ea9
DESKTOP-HL5MIJ7\tulip <tuliphan91@gmail.com>
2022-10-21 17:59:09 +0900

    jpa기본

--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,9 +1,18 @@
 package hello.hellospring.domain;
+import javax.persistence.*;
 
 
 //도메인에는 멤버 클래스를 만든다.
+
+
+@Entity
 public class Member {
+    //@id는 pk값이라고 지정해주는 것!
+    //db에 넣으면 db가 아이디를 자동으로 생성해주는 것을 아이덴티티 전략이라고 함.
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; //시스템 아이디
+
+    @Column(name= "username")
     private String name;
 
     public Long getId() {
---
+++ src/main/java/hello/hellospring/service/SpringConfig.java
@@ -0,0 +1,32 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+//
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+    @Bean
+    public MemberRepository memberRepository() {
+        return new JpaMemberRepository();
+    }
+}
