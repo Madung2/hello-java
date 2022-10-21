c06f150d6115f7a243ce172b8fdbf1c0061322ac
DESKTOP-HL5MIJ7\tulip <tuliphan91@gmail.com>
2022-10-21 13:47:06 +0900

    로그인2

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -2,21 +2,19 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
+    //해시맵은 파이선 딕셔너리. 딕셔너리 타입의 스토어 객체를 만든다.
     private static long sequence =0L; //sequence=012같은 키값을 생성해줌
 
     @Override //부모를 상속할때 맘에 안드는걸 재정의함!!
     public Member save(Member member) {
         member.setId(++sequence); //아이디가 하나씩 올라가게 만듬
         store.put(member.getId(), member); //맵에 저장됨
-        return null;
+        return member;
     }
 
     @Override
@@ -35,6 +33,7 @@ public class MemoryMemberRepository implements MemberRepository {
 
     @Override
     public List<Member> findAll() {
-        return null;
+        return new ArrayList<>(store.values());
+        //스토어에 있는 모든 밸류를 반환한다.
     }
 }
